820ebfa9db7a8d7983adf71d129615f051c3cc8b
Renan Castro <renanccastro@gmail.com>
2021-06-24 11:14:04 -0300

    new version with new meteor release

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'quave:apple-oauth',
-    version: '2.0.0',
+    version: '2.0.1',
     summary: 'Sign in with Apple OAuth flow - fork from bigowl',
     git: 'https://github.com/quavedev/apple-oauth',
 });
